75b3f659e3c5e37898cb8b04744309257436a17d
RiccardoRossetto <riccardo.rossetto.4@gmail.com>
2020-12-04 16:04:37 +0100

    UniswapLiquidity function

--- contracts/UniswapLiquidity.sol
+++ contracts/UniswapLiquidity.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.6.0;
 
 import "./ERC20RMM.sol";
-import "/home/joukowski/node_modules/@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
-
+import "/home/joukowski/node_modules/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract UniswapLiquidity {
 	
-	UniswapV2Router02 routerUni = UniswapV2Router02("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-        ERC20RMM tokenRMM = ERC20RMM("0xE96888C5EeD88ff5f2a1e4a167cA8A3D35DD382f");	
+	address internal constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+	IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
+
+	address internal constant TOKEN_ADDRESS = 0xE96888C5EeD88ff5f2a1e4a167cA8A3D35DD382f;
+        ERC20RMM tokenRMM = ERC20RMM(TOKEN_ADDRESS);	
 	
 	mapping(address => uint) public tokenBalances;
 	mapping(address => bool) public addressExists;
@@ -24,5 +26,7 @@ contract UniswapLiquidity {
 		} else if (addressExists[msg.sender]) {tokenBalances[msg.sender] += _amount;}
 
 	}
+
+	function addLiquidity()
 
 }
