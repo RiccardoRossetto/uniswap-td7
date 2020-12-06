c0249de016298648558c4062c3ba1844a5605431
RiccardoRossetto <riccardo.rossetto.4@gmail.com>
2020-12-06 23:24:31 +0100

    implemented addLiquidityETH test

--- contracts/UniswapLiquidity.sol
+++ contracts/UniswapLiquidity.sol
@@ -8,39 +8,48 @@ contract UniswapLiquidity {
 	address internal constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 	IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
 
-	address internal constant TOKEN_ADDRESS = 0xE96888C5EeD88ff5f2a1e4a167cA8A3D35DD382f;
-        ERC20RMM tokenRMM = ERC20RMM(TOKEN_ADDRESS);	
+	address internal constant TOKEN_ADDRESS = 0x637AD4164afAD756b0f968C14d48970D55F352fC;
+        ERC20RMM tokenRMM = ERC20RMM(TOKEN_ADDRESS);
+	
+	uint[] public balances;
 	
 	mapping(address => uint) public tokenBalances;
+	mapping(address => uint) public etherBalances;
 	mapping(address => bool) public addressExists;
-
-	function depositToken(uint _amount) public {
+	
+	function depositTokens(uint _amount) public {
 		
 		tokenRMM.transferFrom(msg.sender, address(this), _amount);
-		
-		if (!addressExists[msg.sender]){
-			
- 			tokenBalances[msg.sender] = _amount;
-			addressExists[msg.sender] = true;
-
-		} else if (addressExists[msg.sender]) {tokenBalances[msg.sender] += _amount;}
+		tokenBalances[msg.sender] += _amount;		
 
 	}
 
+	function depositEther() payable public {
+
+		etherBalances[msg.sender] += msg.value;
+	}
+
+
 
 	function addLiquidity(
-		uint _amountTokenDesired, 
-		uint _amountTokenMin, 
-		uint _amountETHMin, 
+		uint _amountETHDesired,
+		uint _amountTokenDesired,
+		uint _amountTokenMin,
+		uint _amountETHMin,
+		address _to,
 		uint _deadline
 		) public {
-		uniswapRouter.addLiquidityETH(
-			TOKEN_ADDRESS, 
-			_amountTokenDesired, 
-			_amountTokenMin, 
-			_amountETHMin, 
-			msg.sender, 
-			_deadline);
+		(uint _tokens, uint _ether, uint _liquidity) = uniswapRouter.addLiquidityETH{value: _amountETHDesired}(
+					TOKEN_ADDRESS, 
+					_amountTokenDesired, 
+					_amountTokenMin, 
+					_amountETHMin, 
+					_to,
+					_deadline
+					);
+
+		tokenBalances[msg.sender] -= _tokens;
+		etherBalances[msg.sender] -= _ether;
 	}
 
 	function removeLiquidity(
