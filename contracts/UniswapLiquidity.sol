b91448bd740aca191fc109ac81605396f6130a7f
RiccardoRossetto <riccardo.rossetto.4@gmail.com>
2020-12-08 11:18:17 +0100

    test removeLiquidity

--- contracts/UniswapLiquidity.sol
+++ contracts/UniswapLiquidity.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.0;
 
 import "./ERC20RMM.sol";
+import "/usr/local/lib/node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "/home/joukowski/node_modules/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract UniswapLiquidity {
@@ -10,24 +11,27 @@ contract UniswapLiquidity {
 
 	address internal constant TOKEN_ADDRESS = 0x637AD4164afAD756b0f968C14d48970D55F352fC;
         ERC20RMM tokenRMM = ERC20RMM(TOKEN_ADDRESS);
+
+	address internal constant TOKEN_ADDRESS_LP = 0x12d6e09766221048e27495EB04134B7c952FD016;
+	IERC20 tokenLP = IERC20(TOKEN_ADDRESS_LP);	
 	
-	uint[] public balances;
-	
-	mapping(address => uint) public tokenBalances;
 	mapping(address => uint) public liquidityBalances;
-	mapping(address => uint) public etherBalances;
+	mapping(address => uint) public tokenBalancesInContract;
+	mapping(address => uint) public etherBalancesInContract;
+	mapping(address => uint) public tokenBalancesLiquidity;
+	mapping(address => uint) public etherBalancesLiquidity;
 	mapping(address => bool) public addressExists;
 	
 	function depositTokens(uint _amount) public {
 		
 		tokenRMM.transferFrom(msg.sender, address(this), _amount);
-		tokenBalances[msg.sender] += _amount;		
+		tokenBalancesInContract[msg.sender] += _amount;		
 
 	}
 
 	function depositEther() payable public {
 
-		etherBalances[msg.sender] += msg.value;
+		etherBalancesInContract[msg.sender] += msg.value;
 	}
 
 
@@ -36,11 +40,11 @@ contract UniswapLiquidity {
 		uint _amountETHDesired,
 		uint _amountTokenDesired,
 		uint _amountTokenMin,
-		uint _amountETHMin,
-		uint _deadline
+		uint _amountETHMin
 		) public {
 		
-		address _to = msg.sender;		
+		address _to = msg.sender;
+		uint _deadline = now + 300;		
 	
 		tokenRMM.approve(UNISWAP_ROUTER_ADDRESS, _amountTokenDesired);
 		
@@ -53,46 +57,50 @@ contract UniswapLiquidity {
 					_deadline);
 		
 		liquidityBalances[msg.sender] += _liquidity;
-		tokenBalances[msg.sender] -= _tokens;
-		etherBalances[msg.sender] -= _ether;
+		tokenBalancesInContract[msg.sender] -= _tokens;
+		etherBalancesInContract[msg.sender] -= _ether;
+		tokenBalancesLiquidity[msg.sender] += _tokens;
+		etherBalancesLiquidity[msg.sender] += _ether;
 	}
 
 
 	function removeLiquidity(
-		uint _amountTokenMin,
-		uint _amountETHMin,
-		uint _deadline
 		) public {
 
 		address _to = msg.sender;
 		uint _liquidity = liquidityBalances[msg.sender];
-		
+		uint _amountTokenMin = tokenBalancesLiquidity[msg.sender];
+		uint _amountETHMin = etherBalancesLiquidity[msg.sender];
+		uint _deadline = now + 300;		
+
+		tokenLP.approve(UNISWAP_ROUTER_ADDRESS, tokenLP.balanceOf(msg.sender));
 
 		(uint _tokens, uint _ether) = uniswapRouter.removeLiquidityETH(
 			TOKEN_ADDRESS,
 			_liquidity,
-			_amountTokenMin,	
-			_amountETHMin,
+			_amountTokenMin - 100,	
+			_amountETHMin - 100,
 			_to,
 			_deadline);
 
-		tokenBalances[msg.sender] += _tokens;
-		etherBalances[msg.sender] += _ether;
-		liquidityBalances[msg.sender] -= _liquidity;
+		liquidityBalances[msg.sender] = 0;
+	//	tokenBalancesLiquidity[msg.sender] -= _tokens;
+	//	etherBalancesLiquidity[msg.sender] -= _ether;
+
 
 	}
 
 	
 	function withdrawAll() public {
-		uint _tokens = tokenBalances[msg.sender];
-		uint _ether = etherBalances[msg.sender];
+		uint _tokens = tokenBalancesInContract[msg.sender];
+		uint _ether = etherBalancesInContract[msg.sender];
 		address payable _to = msg.sender;
 
 		_to.transfer(_ether);
 		tokenRMM.transfer(_to, _tokens);
 
-		tokenBalances[msg.sender] -= _tokens;
-		etherBalances[msg.sender] -= _ether;
+		tokenBalancesInContract[msg.sender] -= _tokens;
+		etherBalancesInContract[msg.sender] -= _ether;
 		
 	} 
 
